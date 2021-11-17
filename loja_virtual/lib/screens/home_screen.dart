cedcffea343d77ddacfe82236ca80eee5301e8a2
Gustavo Nogueira <guxavonogueira@gmail.com>
2021-11-17 15:44:12 -0300

    Screen my orders.

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/category_tab.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
@@ -28,10 +29,15 @@ class HomeScreen extends StatelessWidget {
           floatingActionButton: CartButton(),
         ),
         Container(
-          color: Colors.blue,
+          color: Colors.yellow,
         ),
-        Container(
-          color: Colors.green,
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         )
       ],
     );
